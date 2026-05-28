c3c0e60df95dd531de94d653f7bb56c98480b618
CDiaz <cdiaz@transborder.com.co>
2026-05-28 08:12:14 -0500

    initial project

---
+++ src/main/java/com/cristiandev/standard_project/StandardProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cristiandev.standard_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StandardProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StandardProjectApplication.class, args);
+	}
+
+}
